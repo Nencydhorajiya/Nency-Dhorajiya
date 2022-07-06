74ccc9af6c63023b6383858ade404dff268edd6a
nency <mystrongheart02@gmail.com>
2022-07-06 09:54:28 +0530

    c lan.

---
+++ 1.c
@@ -0,0 +1,55 @@
+/* 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n 42+(30+47-20) = %d",42+(30+47-20));
+    return 0;
+}
+*/
+
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=10,b=20,z;
+    printf("\n a = %d",a);
+    printf("\n b = %d",b);
+    printf("\n a+b = %d",a+b);
+    z=a+b;
+   // printf("\n a+b = %d",z);   //a+b=z=30
+    printf("\n z = %d",z);  
+}
+*/
+
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=20,b=10,c=5,z,y,o,p,q;
+    printf("\n a = %d",a);
+    printf("\n b = %d",b);
+    printf("\n c = %d",c);
+
+    printf("\n\n a+b = %d",a+b);
+    printf("\n a+b+c = %d",a+b+c);
+    printf("\n a-b = %d",a-b);
+    printf("\n a-b-c = %d",a-b-c);
+    printf("\n a/c+1 = %d",a/c+1);
+
+    z=a+b;
+    y=a+b+c;
+    o=a-b;
+    p=a-b-c;
+    q=a/c+1;
+    
+   // printf("\n a+b = %d",z);   //a+b = z = 30
+    printf("\n\n z = %d",z);     
+    printf("\n y = %d",y);
+    printf("\n o = %d",o);
+    printf("\n p = %d",p);
+    printf("\n q = %d",q);
+}
+*/
